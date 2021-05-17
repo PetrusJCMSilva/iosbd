89476b763cab0a7a458da20a1c7fb28ccaa3460d
PetrusJCMSilva <72948756+PetrusJCMSilva@users.noreply.github.com>
2021-05-17 00:02:13 -0300

    Add files via upload

---
+++ Carangas/Carangas/Controllers/REST.swift
@@ -0,0 +1,202 @@
+//
+//  REST.swift
+//  Carangas
+//
+//  Created by Douglas Frari on 5/10/21.
+//  Copyright © 2021 Eric Brito. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
+enum RESTOperation {
+    case save
+    case update
+    case delete
+}
+
+
+class REST {
+    
+    // URL + endpoint
+    private static let basePath = "https://carangas.herokuapp.com/cars"
+    
+    // URL TABELA FIPE
+    private static let urlFipe = "https://fipeapi.appspot.com/api/1/carros/marcas.json"
+    
+    // session criada automaticamente e disponivel para reusar
+    private static let session = URLSession(configuration: configuration)
+    
+    private static let configuration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = true
+        config.httpAdditionalHeaders = ["Content-Type":"application/json"]
+        config.timeoutIntervalForRequest = 10.0
+        config.httpMaximumConnectionsPerHost = 5
+        return config
+    }()
+    
+    
+    
+    class func delete(car: Car, onComplete: @escaping (Bool) -> Void ) {
+        applyOperation(car: car, operation: .delete, onComplete: onComplete )
+    }
+    
+    class func update(car: Car, onComplete: @escaping (Bool) -> Void ) {
+        applyOperation(car: car, operation: .update, onComplete: onComplete)
+    }
+    
+    class func save(car: Car, onComplete: @escaping (Bool) -> Void ) {
+        applyOperation(car: car, operation: .save, onComplete: onComplete)
+    }
+    
+    // o metodo pode retornar um array de nil se tiver algum erro
+    class func loadBrands(onComplete: @escaping ([Brand]?) -> Void) {
+        
+        guard let url = URL(string: urlFipe) else {
+            onComplete(nil)
+            return
+        }
+        // tarefa criada, mas nao processada
+        session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onComplete(nil)
+                    return
+                }
+                if response.statusCode == 200 {
+                    // obter o valor de data
+                    guard let data = data else {
+                        onComplete(nil)
+                        return
+                    }
+                    do {
+                        let brands = try JSONDecoder().decode([Brand].self, from: data)
+                        onComplete(brands)
+                    } catch {
+                        // algum erro ocorreu com os dados
+                        onComplete(nil)
+                    }
+                } else {
+                    onComplete(nil)
+                }
+            } else {
+                onComplete(nil)
+            }
+        }.resume()
+        
+    } // fim do loadBrands
+    
+    
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        
+        guard let url = URL(string: REST.basePath) else {
+            onError(.url)
+            return
+        }
+        
+        AF.request(REST.basePath).responseJSON { af in
+            
+           
+            
+            if af.error == nil {
+                            
+                            // algo válido foi retornado do servidor...
+                            
+                            // 2
+                guard let response: URLRequest = af.request else {
+                                onError(.noResponse)
+                                return
+                            }
+                            
+                if af.response?.statusCode == 200 {
+                                
+                                // servidor respondeu com sucesso :)
+                                
+                                // 3
+                                // obter o valor de data
+                                guard let data = af.data else {
+                                    // ERROR porque o data é invalido
+                                    onError(.noData)
+                                    return
+                                }
+                                
+                              
+                                    switch af.result {
+                                        case .success(let value as [Car]):
+                                                    onComplete(value) // SUCESSO
+
+                                        case .failure(let error): break
+                                    
+                                        default:
+                                            onError(.noResponse)
+                                            fatalError("received non-dictionary JSON response")
+                                        
+                    
+                                    }
+                       
+                   
+                }
+                }
+            }
+        }
+    
+    
+    
+    private class func applyOperation(car: Car, operation: RESTOperation , onComplete: @escaping (Bool) -> Void ){
+        
+        // o endpoint do servidor para update é: URL/id
+        let urlString = basePath + "/" + (car._id ?? "")
+        
+        guard let url = URL(string: urlString) else {
+            onComplete(false)
+            return
+        }
+        var request = URLRequest(url: url)
+        var httpMethod: HTTPMethod
+        
+        switch operation {
+        case .delete:
+            httpMethod = .delete
+        case .save:
+            httpMethod = .post
+        case .update:
+            httpMethod = .put
+        }
+        //request.httpMethod = httpMethod
+        
+        // transformar objeto para um JSON, processo contrario do decoder -> Encoder
+        guard let json = try? JSONEncoder().encode(car) else {
+            onComplete(false)
+            return
+        }
+        request.httpBody = json
+        
+        AF.request(url, method: httpMethod, encoding: JSONEncoding.default)
+                        .responseJSON { response in
+                            if response.error == nil {
+              
+                
+                // ok
+                onComplete(true)
+                
+            } else {
+                onComplete(false)
+            }
+            
+        }
+        
+    }
+    
+}
